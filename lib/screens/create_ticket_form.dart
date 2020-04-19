ac3c3e4f5975da8a82372e82acc3aca2674ed15d
shahidan <alipitchay2006@gmail.com>
2020-04-19 15:12:45 +0800

    add attachment functionality

--- lib/screens/create_ticket_form.dart
+++ lib/screens/create_ticket_form.dart
@@ -1,8 +1,10 @@
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:ticketing_app/model/ticket.dart';
 import 'package:intl/intl.dart';
 import 'package:ticketing_app/util/constants.dart';
+import 'package:flutter/services.dart';
 
 class CreateTicketForm extends StatefulWidget {
   @override
@@ -10,11 +12,32 @@ class CreateTicketForm extends StatefulWidget {
 }
 
 class _CreateTicketFormState extends State<CreateTicketForm> {
+  String _fileName;
   Ticket ticket = new Ticket();
   final format = DateFormat("dd-MM-yyyy");
+  String _path;
+  Map<String, String> _paths;
+  bool _loadingPath = false;
 //  bool _autoValidate = true;
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
+  void _openFileExplorer() async {
+    setState(() => _loadingPath = true);
+    try {
+      _path = null;
+      _paths = await FilePicker.getMultiFilePath(
+          type: FileType.custom, allowedExtensions: ['doc', 'pdf', 'xlsx']);
+    } on PlatformException catch (e) {
+      print("Unsupported operation" + e.toString());
+    }
+    if (!mounted) return;
+    setState(() {
+      _loadingPath = false;
+      _fileName = _path != null
+          ? _path.split('/').last
+          : _paths != null ? _paths.keys.toString() : '...';
+    });
+  }
 
   void validateFormFields() {
     if (_formKey.currentState.validate()) {
@@ -46,6 +69,23 @@ class _CreateTicketFormState extends State<CreateTicketForm> {
 //    }
   }
 
+  List<Widget> buildAttachments(Map<String, String> paths) {
+    List<Widget> attachments = [];
+
+    attachments.add(Card(
+      child: ListTile(
+        leading: Text('hello'),
+        trailing: GestureDetector(
+            onTap: (){setState(() {
+              print('here');
+              attachments.removeAt(0);
+            });},
+            child: Icon(Icons.close)),
+      ),
+    ));
+    return attachments;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,125 +105,128 @@ class _CreateTicketFormState extends State<CreateTicketForm> {
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Column(
               children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                      hintText: 'Key in a title to your ticket',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) => val.isEmpty ? 'Please add a title' : null,
-                    onSaved: (val) => ticket.title = val,
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    hintText: 'Key in a title to your ticket',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Description',
-                      hintText: 'Key in description of the issue',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add a description' : null,
-                    onSaved: (val) => ticket.description = val,
+                  validator: (val) => val.isEmpty ? 'Please add a title' : null,
+                  onSaved: (val) => ticket.title = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    hintText: 'Key in description of the issue',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Client Reference 1',
-                      hintText: 'Key in a reference',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add a reference' : null,
-                    onSaved: (val) => ticket.clientRef1 = val,
+                  validator: (val) =>
+                      val.isEmpty ? 'Please add a description' : null,
+                  onSaved: (val) => ticket.description = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Client Reference 1',
+                    hintText: 'Key in a reference',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Client Reference 2',
-                      hintText: 'Key in a reference',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add a reference' : null,
-                    onSaved: (val) => ticket.clientRef2 = val,
+                  validator: (val) =>
+                      val.isEmpty ? 'Please add a reference' : null,
+                  onSaved: (val) => ticket.clientRef1 = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Client Reference 2',
+                    hintText: 'Key in a reference',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Postal code',
-                      hintText: 'Key in your postal code (e.g 53100)',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add a postal code (e.g 53100)' : null,
-                    onSaved: (val) => ticket.dcAccessCode = val,
+                  validator: (val) =>
+                      val.isEmpty ? 'Please add a reference' : null,
+                  onSaved: (val) => ticket.clientRef2 = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Postal code',
+                    hintText: 'Key in your postal code (e.g 53100)',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Equipment Location',
-                      hintText: 'Key in equipment location (e.g 14 Robinson Road #08-01A)',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add equipment lcoation (e.g 14 Robinson Road #08-01A)' : null,
-                    onSaved: (val) => ticket.eqLoc = val,
+                  validator: (val) => val.isEmpty
+                      ? 'Please add a postal code (e.g 53100)'
+                      : null,
+                  onSaved: (val) => ticket.dcAccessCode = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Equipment Location',
+                    hintText:
+                        'Key in equipment location (e.g 14 Robinson Road #08-01A)',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Equipment Serial No',
-                      hintText: 'Key in equipment serial no',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add equipment serial no' : null,
-                    onSaved: (val) => ticket.eqSerialNo = val,
+                  validator: (val) => val.isEmpty
+                      ? 'Please add equipment lcoation (e.g 14 Robinson Road #08-01A)'
+                      : null,
+                  onSaved: (val) => ticket.eqLoc = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Equipment Serial No',
+                    hintText: 'Key in equipment serial no',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Equipment Serial No',
-                      hintText: 'Key in equipment serial no',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add equipment serial no' : null,
-                    onSaved: (val) => ticket.eqSerialNo = val,
+                  validator: (val) =>
+                      val.isEmpty ? 'Please add equipment serial no' : null,
+                  onSaved: (val) => ticket.eqSerialNo = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Equipment Serial No',
+                    hintText: 'Key in equipment serial no',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Part No',
-                      hintText: 'Key in part no',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add part no' : null,
-                    onSaved: (val) => ticket.partno = val,
+                  validator: (val) =>
+                      val.isEmpty ? 'Please add equipment serial no' : null,
+                  onSaved: (val) => ticket.eqSerialNo = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Part No',
+                    hintText: 'Key in part no',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Brand Model',
-                      hintText: 'Key in brand model',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) =>
-                    val.isEmpty ? 'Please add brand model' : null,
-                    onSaved: (val) => ticket.brandModel = val,
+                  validator: (val) => val.isEmpty ? 'Please add part no' : null,
+                  onSaved: (val) => ticket.partno = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Brand Model',
+                    hintText: 'Key in brand model',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Contact Details',
-                      hintText: 'Key in contact details',
-                      labelStyle: TextStyle(color: kTextPrimary),
-                      errorStyle: kErrorTextStyle,
-                    ),
-                    validator: (val) => val.isEmpty ? 'Please add contact details' : null,
-                    onSaved: (val) => ticket.locContact = val,
+                  validator: (val) =>
+                      val.isEmpty ? 'Please add brand model' : null,
+                  onSaved: (val) => ticket.brandModel = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Contact Details',
+                    hintText: 'Key in contact details',
+                    labelStyle: TextStyle(color: kTextPrimary),
+                    errorStyle: kErrorTextStyle,
                   ),
+                  validator: (val) =>
+                      val.isEmpty ? 'Please add contact details' : null,
+                  onSaved: (val) => ticket.locContact = val,
+                ),
                 DateTimeField(
                   decoration: InputDecoration(
                     labelText: 'Service Date',
@@ -202,7 +245,8 @@ class _CreateTicketFormState extends State<CreateTicketForm> {
                   validator: (value) {
                     /// Need to compare if picked date is earlier. Only validate selected current date onwards.
                     DateTime now = DateTime.now();
-                    DateTime currentDate = DateTime(now.year, now.month, now.day);
+                    DateTime currentDate =
+                        DateTime(now.year, now.month, now.day);
                     if (value == null) {
                       return 'Please pick a date';
                     } else if (value.isBefore(currentDate)) {
@@ -212,21 +256,86 @@ class _CreateTicketFormState extends State<CreateTicketForm> {
                   },
                   onSaved: (val) => ticket.srSdateTime = val.toString(),
                 ),
-              SizedBox(
-                height: 10.0,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: RaisedButton(
-                  color: kSubmitButton,
-                  textColor: kTextButton,
-                  onPressed: () {
-                    validateFormFields();
-                  },
-                  child: Text('Submit'),
+                SizedBox(
+                  width: double.infinity,
+                  child: RaisedButton(
+                    color: kSubmitButton,
+                    textColor: kTextButton,
+                    onPressed: () => _openFileExplorer(),
+                    child: Text('Attach files'),
+                  ),
                 ),
-              ),
-                ],
+                Builder(
+                    builder: (BuildContext context) => _loadingPath
+                        ? Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: const CircularProgressIndicator())
+                        : _paths != null
+                            ? Container(
+                                padding: const EdgeInsets.only(bottom: 30.0),
+                                child: Column(
+                                  children: buildAttachments(_paths),
+                                ),
+                              )
+                            : Container()),
+//                new Builder(
+//                  builder: (BuildContext context) => _loadingPath
+//                      ? Padding(
+//                          padding: const EdgeInsets.only(bottom: 10.0),
+//                          child: const CircularProgressIndicator())
+//                      : _path != null || _paths != null
+//                          ? new Container(
+//                              padding: const EdgeInsets.only(bottom: 30.0),
+//                              height: MediaQuery.of(context).size.height * 0.20,
+//                              child: new Scrollbar(
+//                                  child: new ListView.separated(
+//                                itemCount: _paths != null && _paths.isNotEmpty
+//                                    ? _paths.length
+//                                    : 1,
+//                                itemBuilder: (BuildContext context, int index) {
+//                                  final bool isMultiPath =
+//                                      _paths != null && _paths.isNotEmpty;
+////                                  final String name = 'File $index: ' +
+//                                  final String name = (isMultiPath
+//                                      ? _paths.keys.toList()[index]
+//                                      : _fileName ?? '...');
+//                                  final path = isMultiPath
+//                                      ? _paths.values.toList()[index].toString()
+//                                      : _path;
+//
+//                                  return new ListTile(
+//                                    title: new Text(
+//                                      name,
+//                                      style: TextStyle(fontSize: 12.0),
+//                                    ),
+//                                    trailing: Icon(Icons.close),
+////                                    subtitle: new Text(path),
+//                                  );
+//                                },
+//                                separatorBuilder:
+//                                    (BuildContext context, int index) =>
+//                                        new Divider(
+//                                  color: Colors.redAccent,
+//                                ),
+//                              )),
+//                            )
+//                          : new Container(),
+//                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: RaisedButton(
+                    color: kSubmitButton,
+                    textColor: kTextButton,
+                    onPressed: () {
+                      validateFormFields();
+                    },
+                    child: Text('Submit'),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
